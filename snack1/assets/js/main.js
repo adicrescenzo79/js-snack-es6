e0a964a9a8afe6199e957f6669e4cb88b8b02f61
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-03-18 16:59:49 +0100

    completo

--- snack1/assets/js/main.js
+++ snack1/assets/js/main.js
@@ -1,3 +1,57 @@
 $(document).ready(function(){
+  // Creare un array di oggetti:
+  const bikes = [];
+  // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
+  // nome e peso.
+  let nome = '';
+  let peso = '';
 
+  let bike = {
+    nome,
+    peso,
+  }
+
+  nome = 'Mountain-bike'
+  peso = 4
+
+  bike = {
+    nome,
+    peso,
+  }
+
+  bikes.push(bike);
+
+  nome = 'Graziella'
+  peso = 2
+
+  bike = {
+    nome,
+    peso,
+  }
+
+  bikes.push(bike);
+
+  nome = 'City-bike'
+  peso = 3
+
+  bike = {
+    nome,
+    peso,
+  }
+
+  bikes.push(bike);
+
+
+  console.log(bikes);
+  // Stampare a schermo la bici con peso minore utilizzando destructuring
+  // e template literal
+  let index = 0;
+  let min = bikes[0].peso
+  for (let i = 1; i < bikes.length; i++) {
+    if (bikes[i].peso < min) {
+      min = bikes[i].peso;
+      index = i;
+    }
+  }
+  console.log(`La bicicletta più leggera è la ${bikes[index].nome} che pesa ${bikes[index].peso} Kg`);
 });
--- snack2/assets/js/main.js
+++ snack2/assets/js/main.js
@@ -1,3 +1,74 @@
 $(document).ready(function(){
+  // Creare un array di oggetti di squadre di calcio.
+  // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+  const squadre = [];
+
+  // Nome sarà l’unica proprietà da compilare,
+  // le altre saranno tutte settate a 0.
+  // Generare numeri random al posto degli 0 nelle proprietà:
+  // Punti fatti e falli subiti.
+  let squadra = {
+    nome: 'Inter',
+    peso: randomNumberInRange(0, 15),
+    falli: randomNumberInRange(0, 10),
+  }
+
+  squadre.push(squadra);
+
+  squadra = {
+    nome: 'Milan',
+    peso: randomNumberInRange(0, 15),
+    falli: randomNumberInRange(0, 10),
+  }
+
+  squadre.push(squadra);
+
+  squadra = {
+    nome: 'Roma',
+    peso: randomNumberInRange(0, 15),
+    falli: randomNumberInRange(0, 10),
+  }
+
+  squadre.push(squadra);
+
+  squadra = {
+    nome: 'Lazio',
+    peso: randomNumberInRange(0, 15),
+    falli: randomNumberInRange(0, 10),
+  }
+
+  squadre.push(squadra);
+
+  squadra = {
+    nome: 'Juventus',
+    peso: randomNumberInRange(0, 15),
+    falli: randomNumberInRange(0, 10),
+  }
+
+  squadre.push(squadra);
+
+  console.log(squadre);
+
+
+  // Infine usando la destrutturazione creiamo un nuovo array i cui elementi
+  // contengono solo nomi e falli subiti e stampiamo tutto in console.
+  const squadre1 = [];
+
+  for (let i = 0; i < squadre.length; i++) {
+    let {nome, falli} = squadre[i];
+    squadra = {
+      nome,
+      falli,
+    };
+    squadre1.push(squadra);
+    console.log(`La squadra ${nome} ha subito ${falli} falli`);
+  }
+
+  console.log(squadre1);
 
 });
+
+// FUNCTION
+function randomNumberInRange(min, max) {
+  return Math.floor(Math.random() * max) + min;
+}
